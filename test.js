747196db7b58835a143d3766b45fd2e723686b2b
Junior Sanchez <Op.JASG@gmail.com>
2021-08-04 01:15:55 -0300

    02-Commit_RX

--- test.js
+++ test.js
@@ -16,6 +16,86 @@ window.addEventListener('devicemotion', function(event) {
     Acel_z.innerHTML=event.z + ' m/s2';
   });
 
+
+
+  
+
+
+//   THREE JS
+
+
+
+  var renderer,
+  scene,
+  camera,
+  canvas = document.getElementById('canvas');
+
+//RENDERER
+renderer = new THREE.WebGLRenderer({canvas, alpha: true});
+renderer.setClearColor(0xffffff, 0);
+// scene.renderer.setClearColor(0xffffff, 0);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth*0.97, window.innerHeight*0.97);
+
+//CAMERA
+camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 0.9;
+camera.position.y = -0.45;
+
+
+//SCENE
+scene = new THREE.Scene();
+
+var light2 = new THREE.PointLight(0xffffff);
+scene.add(light2);
+
+var loader = new THREE.GLTFLoader();
+
+
+
+loader.load('./RX78.glb', handle_load);
+
+
+
+var object;
+
+
+function handle_load(gltf) {
+
+
+  object = gltf.scene;
+
+  
+  scene.add( object );
+  object.position.z = -50;
+  object.scale.set(0.2, 0.2, 0.2);
+  object.position.set(0, -0.5, 0);
+  
+  light2.lookAt(object.position);
+  light2.position.set(0, 1, 1);
+  //RESIZEas
+  window.addEventListener("resize", redimensionar); 
+  function redimensionar(){
+      camera.aspect= window.innerWidth / window.innerHeight;
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      camera.updateProjectionMatrix();
+      renderer.render(scene, camera);
+  }
+
+//   window.addEventListener("mousemove", (e)=>{
+//       var lookX = 0.05*(e.clientX-(window.innerWidth/2))
+//       var lookY = (-(0.05*(e.clientY-(window.innerHeight/2)))+5)
+//       object.lookAt(lookX,lookY,30);
+//       light2.position.set(0, 1, 1);
+//       // console.log(cube.lookAt);
+//       window.addEventListener("mouseout", (e)=>{
+//           lookX=0;
+//           lookY=0;
+//           light2.position.set(0, 0.5, -0.9);
+//           object.lookAt(lookX,lookY,30)
+//       });
+      
+//   });
 window.addEventListener("deviceorientation", handleOrientation, true);
 
 function handleOrientation(event) {
@@ -26,6 +106,25 @@ function handleOrientation(event) {
     
     Absolute.innerHTML=absolute;
     Alpha.innerHTML=alpha;
-    Beta.innerHTML=beta;
-    Gamma.innerHTML=gamma;
+    var lookY =beta;
+    var lookX =gamma;
+    object.lookAt(lookX,lookY,30);
+    // Beta = Y, Gamma = X
   }
+}
+
+
+//RENDER LOOP
+render();
+
+// var delta = 0;
+// var prevTime = Date.now();
+
+function render() {
+
+  // delta += 0.1;
+
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(render);
+}
